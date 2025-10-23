50f12e9aba3a970d71db8cf92437e22731eb2ccf
actuallyoms <114298412+actuallyoms@users.noreply.github.com>
2025-10-23 22:41:59 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "rg_name" {
+  description = "Resource Group name"
+  type        = string
+  default     = "tf-test-rg"
+}
 
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
+}
