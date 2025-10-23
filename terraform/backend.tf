0e2752d97961a6034341a3c199dc8dbc1734f695
actuallyoms <114298412+actuallyoms@users.noreply.github.com>
2025-10-23 22:39:54 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstatesa-rg"    # senin state RG'si
+    storage_account_name = "tfstatedevopsomer"     # senin Storage Account adı
+    container_name       = "tfstate"              # senin container adı
+    key                  = "terraform.tfstate"    # state dosyasının blob içindeki adı
+  }
+}
