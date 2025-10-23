e0097a4fb0ef345309a79b62ed7407b0acc4cfc3
actuallyoms <114298412+actuallyoms@users.noreply.github.com>
2025-10-23 22:53:29 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,9 @@ resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
 }
+
+resource "azurerm_resource_group" "rg2" {
+  name     = var.rg_name
+  location = var.location
+}
+
