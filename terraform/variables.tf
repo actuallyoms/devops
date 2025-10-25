26796c350e3365ea5f5b076845201f30d1dc445c
actuallyoms <114298412+actuallyoms@users.noreply.github.com>
2025-10-25 18:37:39 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,19 @@
-variable "rg_name" {
-  description = "Resource Group name"
-  type        = string
-  default     = "tf-test-rg"
-}
 
 variable "location" {
-  description = "Azure region"
+  description = "Azure lokasyonu"
   type        = string
   default     = "westeurope"
+}
+
+
+variable "rg_name" {
+  description = "Resource Group adı"
+  type        = string
+}
+
+
+variable "aks_name" {
+  description = "AKS cluster adı"
+  type        = string
+  default     = "omer-aks-cluster"
 }
