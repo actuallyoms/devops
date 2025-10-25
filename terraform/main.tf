33371c5850108d721bf8415a16de441da398e385
actuallyoms <114298412+actuallyoms@users.noreply.github.com>
2025-10-25 19:00:09 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,8 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.aks_name
-  location            = azurerm_resource_group.rg.location  # RG lokasyonu
-  resource_group_name = azurerm_resource_group.rg.name      # RG adı
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "${var.aks_name}-dns"
 
   # Default/System Node Pool (zorunlu)
@@ -21,6 +21,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
     name       = "systempool"
     vm_size    = "Standard_DS2_v2"
     node_count = 1
+
+    node_labels = {
+      tier = "production"
+    }
+  }
 
   # Identity
   identity {
@@ -46,19 +51,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# USER NODE POOL (3 worker node)
-resource "azurerm_kubernetes_cluster_node_pool" "workers" {
-  name                  = "workerpool"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
-  vm_size               = "Standard_DS2_v2"
-  node_count            = 3
-  mode                  = "User"
+# --- Worker Node Pools ---
 
-  # Node bazlı label'lar için ayrı ayrı node pool kullanmamız gerekiyor.
-  # Terraform tek node pool içinde farklı label veremez, o yüzden 3 ayrı pool yapacağız:
-}
-
-# --- Worker 1 ve 2: tier=production ---
+# Worker 1 ve 2: tier=production
 resource "azurerm_kubernetes_cluster_node_pool" "worker_prod" {
   count                 = 2
   name                  = "worker-prod-${count.index + 1}"
@@ -72,7 +67,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "worker_prod" {
   }
 }
 
-# --- Worker 3: tier=test ---
+# Worker 3: tier=test
 resource "azurerm_kubernetes_cluster_node_pool" "worker_test" {
   name                  = "worker-test"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
